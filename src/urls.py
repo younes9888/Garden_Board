c1964249f062d0b7db0321cd997272be989a4eb4
Younes ZIAT <younes88-02@hotmail.com>
2022-10-26 09:11:08 +0200

    update

--- src/urls.py
+++ src/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
    
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
